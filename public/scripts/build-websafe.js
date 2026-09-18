7e761221d4816f672f6f30ae121e852470c0ca59
Arun Kumar Sahlam <arunsahlam@gmail.com>
2026-09-18 18:27:18 +0530

    Delete public directory

--- public/scripts/build-websafe.js
+++
@@ -1,6 +0,0 @@
-const STEPS = ["00", "33", "66", "99", "CC", "FF"];
-const cells = [];
-for (const r of STEPS) for (const g of STEPS) for (const b of STEPS) {
-  const hex = (r + g + b).toUpperCase();
-  cells.push(`<a class="ws-cell" href="/hex/${hex.toLowerCase()}" title="#${hex}">#${hex}</a>`);
-}
